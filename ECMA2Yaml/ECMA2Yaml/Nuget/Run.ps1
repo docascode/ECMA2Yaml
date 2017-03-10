a49446cdd297d5df892824d50785cbf337421386
Tianqi Zhang <tianzh@microsoft.com>
2017-03-10 14:14:30 +0800

    use repo url and branch from build

--- ECMA2Yaml/ECMA2Yaml/Nuget/Run.ps1
+++ ECMA2Yaml/ECMA2Yaml/Nuget/Run.ps1
@@ -11,26 +11,18 @@ $errorActionPreference = 'Stop'
 $source = $($MyInvocation.MyCommand.Definition)
 
 $repositoryRoot = $ParameterDictionary.environment.repositoryRoot
-$logLevel = $ParameterDictionary.environment.logLevel
 $logFilePath = $ParameterDictionary.environment.logFile
-$buildOutputSubfolder = $ParameterDictionary.docset.buildOutputSubfolder
 $logOutputFolder = $currentDictionary.environment.logOutputFolder
-$dependencyPackages = $ParameterDictionary.environment.packages
 
 $dependentFileListFilePath = $ParameterDictionary.context.dependentFileListFilePath
 $changeListTsvFilePath = $ParameterDictionary.context.changeListTsvFilePath
 $userSpecifiedChangeListTsvFilePath = $ParameterDictionary.context.userSpecifiedChangeListTsvFilePath
 
-$currentBranch = 'master'
-git branch | foreach {
-    if ($_ -match "^\* (.*)") {
-        $currentBranch += $matches[1]
-    }
-}
+$repoUrl = $ParameterDictionary.environment.repositoryOriginUrl -replace "\.git$", "/"
+$ecmaXmlGitUrlBase = $repoUrl + "blob/" + $ParameterDictionary.environment.repositoryCurrentBranch
+echo "Using $ecmaXmlGitUrlBase as url base"
 
 $ecmaConfig = $ParameterDictionary.environment.publishConfigContent.ECMA2Yaml
-$ecmaXmlGitUrlBase = $ecmaConfig.RepoUrl + "blob/" + $currentBranch
-echo "Using $ecmaXmlGitUrlBase as url base"
 $ecmaSourceXmlFolder = Join-Path $repositoryRoot $ecmaConfig.SourceXmlFolder
 $ecmaOutputYamlFolder = Join-Path $repositoryRoot $ecmaConfig.OutputYamlFolder
 $allArgs = @("-s", "$ecmaSourceXmlFolder", "-o", "$ecmaOutputYamlFolder", "-l", "$logFilePath", "-p", """$repositoryRoot=>$ecmaXmlGitUrlBase""");
