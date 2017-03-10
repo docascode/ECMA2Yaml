d463a86bd3bf1b86478f51e6ee3b0739e97b8d8d
Tianqi Zhang <TianqiZhang@users.noreply.github.com>
2017-03-10 21:39:41 +0800

    Update Program.cs

--- ECMA2Yaml/ECMA2Yaml/Program.cs
+++ ECMA2Yaml/ECMA2Yaml/Program.cs
@@ -1,4 +1,4 @@
-﻿using ECMA2Yaml.Models;
+using ECMA2Yaml.Models;
 using Microsoft.DocAsCode.Common;
 using Microsoft.DocAsCode.DataContracts.ManagedReference;
 using System;
@@ -83,7 +83,12 @@ namespace ECMA2Yaml
                 var nsFolder = Path.Combine(opt.OutputFolder, ns.Key);
                 var nsFileName = Path.Combine(opt.OutputFolder, ns.Key + ".yml");
                 YamlUtility.Serialize(nsFileName, nsPages[ns.Key], YamlMime.ManagedReference);
-                fileMapping.TryAdd(ns.Value.Metadata[OPSMetadata.XMLLocalPath].ToString(), nsFileName);
+                if (ns.Value.Metadata.ContainsKey(OPSMetadata.XMLLocalPath))
+                {
+                    fileMapping.TryAdd(ns.Value.Metadata[OPSMetadata.XMLLocalPath].ToString(), nsFileName);
+                    ns.Value.Metadata.Remove(OPSMetadata.XMLLocalPath);
+                }
+                
                 if (!opt.Flatten)
                 {
                     if (!Directory.Exists(nsFolder))
@@ -97,7 +102,11 @@ namespace ECMA2Yaml
                     var typePage = typePages[t.Uid];
                     var tFileName = Path.Combine(opt.Flatten ? opt.OutputFolder : nsFolder, t.Uid.Replace('`', '-') + ".yml");
                     YamlUtility.Serialize(tFileName, typePage, YamlMime.ManagedReference);
-                    fileMapping.TryAdd(t.Metadata[OPSMetadata.XMLLocalPath].ToString(), tFileName);
+                    if (t.Metadata.ContainsKey(OPSMetadata.XMLLocalPath))
+                    {
+                        fileMapping.TryAdd(t.Metadata[OPSMetadata.XMLLocalPath].ToString(), tFileName);
+                        t.Metadata.Remove(OPSMetadata.XMLLocalPath);
+                    }
                 }
             });
             YamlUtility.Serialize(Path.Combine(opt.OutputFolder, "toc.yml"), TOCGenerator.Generate(store), YamlMime.TableOfContent);
