d12fa0b09f19d3d8f8b5368a676702103b594e66
Tianqi Zhang <tianzh@microsoft.com>
2017-04-14 13:30:09 +0800

    add full name for overload in references

--- ECMA2Yaml/ECMAHelper/Models/ECMAStore.cs
+++ ECMA2Yaml/ECMAHelper/Models/ECMAStore.cs
@@ -263,6 +263,7 @@ namespace ECMA2Yaml.Models
                     }
                     overloads[m.Name].Id = id;
                     overloads[m.Name].DisplayName = m.ItemType == ItemType.Constructor ? t.Name : m.Name;
+                    overloads[m.Name].FullDisplayName = t.FullName + "." + overloads[m.Name].DisplayName;
                     overloads[m.Name].SourceFileLocalPath = m.SourceFileLocalPath;
                     m.Overload = overloads[m.Name].Uid;
                 }
