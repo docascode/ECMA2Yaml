296c13dc08c1a9e414ffd9551a6729389fa521cc
Tianqi Zhang <tianzh@microsoft.com>
2017-06-30 10:57:25 +0800

    support docset config in post build script

--- ECMA2Yaml/ECMA2Yaml/Nuget/PostBuild.ps1
+++ ECMA2Yaml/ECMA2Yaml/Nuget/PostBuild.ps1
@@ -8,17 +8,28 @@ $errorActionPreference = 'Stop'
 $currentDir = $($MyInvocation.MyCommand.Definition) | Split-Path
 $repositoryRoot = $ParameterDictionary.environment.repositoryRoot
 $outputFolder = $currentDictionary.environment.outputFolder
-
-$ecmaConfig = $ParameterDictionary.environment.publishConfigContent.ECMA2Yaml
-$ecmaOutputYamlFolder = Join-Path $repositoryRoot $ecmaConfig.OutputYamlFolder
-$ecmaOutputMDFolder = Join-Path $ecmaOutputYamlFolder "overwrites"
-
-$ymlOutputFolder = Join-Path $outputFolder "_yml"
-$mdOutputFolder = Join-Path $ymlOutputFolder "overwrites"
-& robocopy $ecmaOutputYamlFolder $ymlOutputFolder *.yml /s
-& robocopy $ecmaOutputMDFolder $mdOutputFolder *.md /s
-if ($LASTEXITCODE -ne 1)
+$jobs = $ParameterDictionary.docset.docsetInfo.ECMA2Yaml
+if (!$jobs)
 {
-    exit $LASTEXITCODE
+	$jobs = $ParameterDictionary.environment.publishConfigContent.ECMA2Yaml
+}
+if ($jobs -isnot [system.array])
+{
+    $jobs = @($jobs)
+}
+foreach($ecmaConfig in $jobs)
+{
+	$ecmaOutputYamlFolder = Join-Path $repositoryRoot $ecmaConfig.OutputYamlFolder
+	$ecmaOutputMDFolder = Join-Path $ecmaOutputYamlFolder "overwrites"
+
+	$ymlOutputFolder = Join-Path $outputFolder "_yml"
+	$mdOutputFolder = Join-Path $ymlOutputFolder "overwrites"
+	& robocopy $ecmaOutputYamlFolder $ymlOutputFolder *.yml /s
+	& robocopy $ecmaOutputMDFolder $mdOutputFolder *.md /s
+
+	if ($LASTEXITCODE -ne 1)
+	{
+		exit $LASTEXITCODE
+	}
 }
 exit 0
