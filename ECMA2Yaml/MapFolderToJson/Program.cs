09dba147f700aaea34c4ee2de4bca39491d148d1
Tianqi Zhang <tianzh@microsoft.com>
2018-05-03 10:09:21 +0800

    fix facade assembly detection

--- ECMA2Yaml/MapFolderToJson/Program.cs
+++ ECMA2Yaml/MapFolderToJson/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Reflection;
@@ -11,57 +12,38 @@ namespace MapFolderToJson
 {
     class Program
     {
-        static string currectFolder = null;
+        
         static void Main(string[] args)
         {
-            AppDomain.CurrentDomain.ReflectionOnlyAssemblyResolve += new ResolveEventHandler(CurrentDomain_ReflectionOnlyAssemblyResolve);
+            AppDomain.CurrentDomain.ReflectionOnlyAssemblyResolve += new ResolveEventHandler(AssemblyLoader.CurrentDomain_ReflectionOnlyAssemblyResolve);
+            var settings = new AppDomainSetup
+            {
+                ApplicationBase = AppDomain.CurrentDomain.BaseDirectory,
+            };
             if (Directory.Exists(args[0]))
             {
                 List<Tuple<string, string>> MonikerAssemblyPairs = new List<Tuple<string, string>>();
                 foreach(var monikerFolder in Directory.GetDirectories(args[0]))
                 {
-                    currectFolder = monikerFolder;
-                    var monikerName = Path.GetFileName(monikerFolder);
-                    foreach(var dll in Directory.GetFiles(monikerFolder, "*.dll"))
-                    {
-                        bool isFacade = false;
-                        try
-                        {
-                            var asm = Assembly.ReflectionOnlyLoadFrom(dll);
-                            isFacade = !asm.DefinedTypes.Any();
-                        }
-                        catch
-                        {
-                            //don't do any thing, real facade dll won't cause this error
-                        }
-                        if (!isFacade)
-                        {
-                            MonikerAssemblyPairs.Add(Tuple.Create(monikerName, Path.GetFileNameWithoutExtension(dll)));
-                        }
-                    }
+                    var childDomain = AppDomain.CreateDomain(Guid.NewGuid().ToString(), null, settings);
+
+                    var handle = Activator.CreateInstance(childDomain,
+                               typeof(AssemblyLoader).Assembly.FullName,
+                               typeof(AssemblyLoader).FullName,
+                               false, BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance, null, null, CultureInfo.CurrentCulture, new object[0]);
+
+                    var loader = (AssemblyLoader)handle.Unwrap();
+
+                    //This operation is executed in the new AppDomain
+                    var paths = loader.LoadExceptFacade(monikerFolder);
+                    MonikerAssemblyPairs.AddRange(paths);
+
+                    AppDomain.Unload(childDomain);
                 }
                 var moniker2Assembly = MonikerAssemblyPairs.GroupBy(t => t.Item1).ToDictionary(g => g.Key, g => g.Select(t => t.Item2).ToArray());
                 //var moniker2Assembly = MonikerAssemblyPairs.ToLookup(t => t.Item1, t => t.Item2);
                 File.WriteAllText("_moniker2Assembly.json", JsonConvert.SerializeObject(moniker2Assembly, Formatting.Indented));
             }
-        }
-
-        static Assembly CurrentDomain_ReflectionOnlyAssemblyResolve(object sender, ResolveEventArgs args)
-        {
-            try
-            {
-                return Assembly.ReflectionOnlyLoad(args.Name);
-            }
-            catch(Exception ex)
-            {
-                var dllName = string.Format("{0}\\{1}.dll", currectFolder, args.Name.Split(',')[0]);
-                if (File.Exists(dllName))
-                {
-                    return Assembly.ReflectionOnlyLoadFrom(dllName);
-                }
-            }
-
-            return null;
         }
     }
 }
