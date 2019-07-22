f44730d00ad8fa0224eaa677e87b7009788d6106
v-susu <v-susu@microsoft.com>
2019-07-22 16:38:14 +0800

    add IntellisenseGen project and member order project

---
+++ ECMA2Yaml/IntellisenseFileGen/Models/Type.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace IntellisenseFileGen.Models
+{
+    public class Type
+    {
+        public string DocId { get; set; }
+        public List<string> AssemblyInfos { get;set;}
+        public List<Member> Members { get; set; }
+        public XElement Docs { get; set; }
+    }
+}
