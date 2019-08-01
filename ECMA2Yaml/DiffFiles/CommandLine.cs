ba1261bfa4f85c48ecfe52fdab4d5c7edf8173c6
luyajun0205 <v-yajlu@microsoft.com>
2019-08-01 16:50:57 +0800

    add yaml diff testing(#22)
    * add diff files tool
    
    * update scripts
    
    * update scripts again
    
    * update according comments
    
    * update script for develop branch
    
    * update scripts
    
    * test diff yaml files with different case
    
    * update difffile tools
    
    * update yaml

---
+++ ECMA2Yaml/DiffFiles/CommandLine.cs
@@ -0,0 +1,24 @@
+﻿using CommandLine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiffFiles
+{
+    public class CommandLine
+    {
+        [Option('o', "OldPath", Required = true, HelpText = "old file path")]
+        public string OldPath { get; set; }
+
+        [Option('n', "NewPath", Required = true, HelpText = "new file path")]
+        public string NewPath { get; set; }
+
+        [Option('l', "LogPath", Required = true, HelpText = "Compare result log path")]
+        public string LogPath { get; set; }
+
+        [Option("Path", Required = true, Default = true,  HelpText = "Compare two path")]
+        public bool IsDiffPath { get; set; }
+    }
+}
---
+++ diffYaml.bat
@@ -0,0 +1,6 @@
+"./ECMA2Yaml/ECMA2Yaml/bin/Release/ECMA2Yaml.exe" -s ./test/xml -o ./test/yml_MREF_After -f
+"./ECMA2Yaml/ECMA2Yaml/bin/Release/ECMA2Yaml.exe" -s ./test/xml -o ./test/yml_SDP_After --SDP -f
+"./ECMA2Yaml/DiffFiles/bin/Release/DiffFiles.exe" -o ./test/yml_MREF -n ./test/yml_MREF_After -l ./test --Path
+IF NOT %ErrorLevel% == 0 ECHO Exist different places for MREF yml
+"./ECMA2Yaml/DiffFiles/bin/Release/DiffFiles.exe" -o ./test/yml_SDP -n ./test/yml_SDP_After -l ./test --Path
+IF NOT %ErrorLevel% == 0 ECHO Exist different places for SDP yml
