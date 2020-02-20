44b2a4ddec77e1059f3c025beb3634ead5506250
Tianqi Zhang <ztq121121@gmail.com>
2020-02-20 16:39:22 +0800

    add logic

---
+++ ECMA2Yaml/ECMA2Yaml_PostBuild/DependencyItem.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECMA2Yaml
+{
+    public class DependencyItem
+    {
+        [JsonProperty("from_file_path")]
+        public string FromFile { get; set; }
+        [JsonProperty("to_file_path")]
+        public string ToFile { get; set; }
+        [JsonProperty("dependency_type")]
+        public string DependencyType { get; set; }
+        [JsonProperty("version")]
+        public string Version { get; set; }
+
+        public void TranslateFileNames(Dictionary<string, string> nameMapping)
+        {
+            if (nameMapping.TryGetValue(FromFile, out string newFromFile))
+            {
+                FromFile = newFromFile;
+            }
+            if (nameMapping.TryGetValue(ToFile, out string newToFile))
+            {
+                ToFile = newToFile;
+            }
+        }
+
+        public string GetUniqueId()
+        {
+            return $"{FromFile}-{ToFile}-{DependencyType}-{Version}";
+        }
+    }
+}
--- ECMA2Yaml/ECMA2Yaml_PostBuild/Program.cs
+++ ECMA2Yaml/ECMA2Yaml_PostBuild/Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -32,7 +34,43 @@ namespace ECMA2Yaml
 
         static void TranslateDependencyFile(CommandLineOptions opt)
         {
+            if (!File.Exists(opt.XMLYamlMappingFile))
+            {
+                OPSLogger.LogUserError(LogCode.ECMA2Yaml_File_LoadFailed, $"{opt.XMLYamlMappingFile} does not exist.");
+            }
+            if (!File.Exists(opt.AggregatedDependencyFile))
+            {
+                OPSLogger.LogUserError(LogCode.ECMA2Yaml_File_LoadFailed, $"{opt.AggregatedDependencyFile} does not exist.");
+            }
 
+            var xmlYamlMapping = JsonConvert.DeserializeObject<Dictionary<string, List<string>>>(File.ReadAllText(opt.XMLYamlMappingFile));
+            var yamlXMLMapping = new Dictionary<string, string>();
+            foreach (var singleXMLMapping in xmlYamlMapping)
+            {
+                foreach (var yamlFile in singleXMLMapping.Value)
+                {
+                    yamlXMLMapping[yamlFile] = singleXMLMapping.Key;
+                }
+            }
+
+            string line;
+            var allDependencies = new List<DependencyItem>();
+            HashSet<string> uniqueSet = new HashSet<string>();
+            using (var file = new StreamReader(opt.AggregatedDependencyFile))
+            {
+                while (!string.IsNullOrWhiteSpace((line = file.ReadLine())))
+                {
+                    var item = JsonConvert.DeserializeObject<DependencyItem>(line);
+                    item.TranslateFileNames(yamlXMLMapping);
+                    var id = item.GetUniqueId();
+                    if (uniqueSet.Add(id))
+                    {
+                        allDependencies.Add(item);
+                    }
+                }
+            }
+            var newLines = allDependencies.Select(item => JsonConvert.SerializeObject(item)).ToArray();
+            File.WriteAllLines(opt.AggregatedDependencyFile, newLines);
         }
     }
 }
