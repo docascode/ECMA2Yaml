a48041b77dfd739c79b5f131821bd90275837d53
Tianqi Zhang <TianqiZhang@users.noreply.github.com>
2020-04-05 18:44:40 +0800

    simplify integration test by removing unversioned SDP (#83)

--- diffYaml.bat
+++ diffYaml.bat
@@ -1,11 +1,7 @@
 set currentDir=%~dp0
 "./ECMA2Yaml/ECMA2Yaml/bin/Release/ECMA2Yaml.exe" -s %currentDir%test\xml -o %currentDir%test\yml_MREF_After -f --repoRoot %currentDir%\ --repoBranch master --repoUrl https://github.com/docascode/ECMA2Yaml
-"./ECMA2Yaml/ECMA2Yaml/bin/Release/ECMA2Yaml.exe" -s %currentDir%test\xml -o %currentDir%test\yml_SDP_After --SDP -f --NoVersioning --repoRoot %currentDir%\ --repoBranch master --repoUrl https://github.com/docascode/ECMA2Yaml
+"./ECMA2Yaml/ECMA2Yaml/bin/Release/ECMA2Yaml.exe" -s %currentDir%test\xml -o %currentDir%test\yml_SDP_After --SDP -f --repoRoot %currentDir%\ --repoBranch master --repoUrl https://github.com/docascode/ECMA2Yaml
 "./ECMA2Yaml/DiffFiles/bin/Release/DiffFiles.exe" -o %currentDir%test\yml_MREF -n %currentDir%test\yml_MREF_After -l %currentDir%test --Path
 IF NOT %ErrorLevel% == 0 ECHO Exist different places for MREF yml
 "./ECMA2Yaml/DiffFiles/bin/Release/DiffFiles.exe" -o %currentDir%test\yml_SDP -n %currentDir%test\yml_SDP_After -l %currentDir%test --Path
 IF NOT %ErrorLevel% == 0 ECHO Exist different places for SDP yml
-
-"./ECMA2Yaml/ECMA2Yaml/bin/Release/ECMA2Yaml.exe" -s %currentDir%test\xml -o %currentDir%test\yml_SDP_versioned_After --SDP -f --repoRoot %currentDir%\ --repoBranch master --repoUrl https://github.com/docascode/ECMA2Yaml
-"./ECMA2Yaml/DiffFiles/bin/Release/DiffFiles.exe" -o %currentDir%test\yml_SDP_versioned -n %currentDir%test\yml_SDP_versioned_After -l %currentDir%test --Path
-IF NOT %ErrorLevel% == 0 ECHO Exist different places for versioned SDP yml
