ea554c54861dd6961ec4c5f7ab42476dc4dd7a3d
Tianqi Zhang <tianzh@microsoft.com>
2020-04-13 17:38:12 +0800

    initial checkin of static helper for DocFx V3

---
+++ ECMA2Yaml/ECMAHelper/ECMA2YamlConverter.cs
@@ -0,0 +1,79 @@
+﻿using ECMA2Yaml.IO;
+using Microsoft.DocAsCode.Common;
+using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Linq;
+using FileAbstractLayer = ECMA2Yaml.IO.FileAbstractLayer;
+
+namespace ECMA2Yaml
+{
+    public class ECMA2YamlConverter
+    {
+        public static void Run(
+            string xmlDirectory,
+            string outputDirectory,
+            string fallbackXmlDirectory = null,
+            string logFilePath = null,
+            string logContentBaseDirectory = null,
+            string sourceMapFilePath = null,
+            ECMA2YamlRepoConfig config = null)
+        {
+            if (xmlDirectory == null)
+            {
+                throw new ArgumentNullException(xmlDirectory);
+            }
+            if (outputDirectory == null)
+            {
+                throw new ArgumentNullException(outputDirectory);
+            }
+            if (!string.IsNullOrEmpty(logContentBaseDirectory))
+            {
+                OPSLogger.PathTrimPrefix = logContentBaseDirectory;
+            }
+
+            try
+            {
+                var fileAccessor = new FileAccessor(xmlDirectory, fallbackXmlDirectory);
+                ECMALoader loader = new ECMALoader(fileAccessor);
+                Console.WriteLine("Loading ECMAXML files...");
+                var store = loader.LoadFolder("");
+                if (store == null)
+                {
+                    return;
+                }
+
+                Console.WriteLine("Building loaded files...");
+                store.StrictMode = true;
+                store.UWPMode = config?.UWP ?? false;
+                store.Build();
+
+                var xmlYamlFileMapping = SDPYamlGenerator.Generate(store, outputDirectory, flatten: config?.Flatten ?? true, withVersioning: true);
+                if (!string.IsNullOrEmpty(sourceMapFilePath))
+                {
+                    var translatedMapping = xmlYamlFileMapping.ToDictionary(
+                        p => FileAbstractLayer.RelativePath(p.Key, sourceMapFilePath),
+                        p => p.Value.Select(path => FileAbstractLayer.RelativePath(path, sourceMapFilePath)).ToList()
+                        );
+                    var json = JsonConvert.SerializeObject(translatedMapping);
+                    File.WriteAllText(sourceMapFilePath, json);
+                }
+
+                var toc = SDPTOCGenerator.Generate(store);
+                YamlUtility.Serialize(Path.Combine(outputDirectory, "toc.yml"), toc, YamlMime.TableOfContent);
+            }
+            catch (Exception ex)
+            {
+                OPSLogger.LogSystemError(LogCode.ECMA2Yaml_InternalError, null, ex.ToString());
+                throw;
+            }
+            finally
+            {
+                if (!string.IsNullOrEmpty(logFilePath))
+                {
+                    OPSLogger.Flush(logFilePath);
+                }
+            }
+        }
+    }
+}
---
+++ ECMA2Yaml/UnitTest/ECMA2YamlConverterTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using ECMA2Yaml;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class ECMA2YamlConverterTests
+    {
+        public TestContext TestContext { get; set; }
+
+        [TestMethod]
+        public void ECMA2YamlConverter_HappyPath()
+        {
+            string repoPath = Path.GetDirectoryName(Path.GetDirectoryName(Path.GetDirectoryName(TestContext.TestDir)));
+            string xmlDirectory = Path.Combine(repoPath, "test\\xml");
+            string outputDirectory = Path.Combine(repoPath, "test\\_yml_UnitTests_ECMA2YamlConverter_HappyPath");
+            string fallbackXmlDirectory = null;
+            string logFilePath = null;
+            string logContentBaseDirectory = null;
+            string sourceMapFilePath = null;
+            ECMA2YamlConverter.Run(xmlDirectory, outputDirectory);
+        }
+    }
+}
