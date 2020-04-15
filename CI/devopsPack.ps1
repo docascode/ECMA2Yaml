cbe9b991e96586303b9e64dacbc06dde805c9fdf
Tianqi Zhang <TianqiZhang@users.noreply.github.com>
2020-04-15 16:52:15 +0800

    Switch to Azure DevOps CI Pipeline (#89)
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * move CI scripts
    
    * fix diff intelisense path
    
    * fix integration test
    
    * fix variable usage
    
    * fix nuget pack path
    
    * nuget push code
    
    * revert changes in appveyor
    
    * fix appveyor again
    
    * fix appveyor
    
    * fix devops version
    
    * fix devops version
    
    * fix nuget push path
    
    * fix nuget path again

---
+++ CI/devopsPack.ps1
@@ -0,0 +1,21 @@
+$revision = ""
+$commitCount = & { git rev-list --count HEAD }
+$version = ''
+$repoRoot = $($MyInvocation.MyCommand.Definition) | Split-Path | Split-Path
+
+if ("$env:BUILD_REASON" -eq "PullRequest" -or "$env:BUILD_SOURCEBRANCH" -eq 'refs/heads/develop') {
+    $version = "1.1.$commitCount-beta$env:BUILD_BUILDNUMBER"
+    Write-Host "##vso[task.setvariable variable=NugetVersionType;]prerelease"
+}
+if (("$env:BUILD_REASON" -eq "Manual" -or "$env:BUILD_REASON" -eq "IndividualCI") `
+    -and "$env:BUILD_SOURCEBRANCH" -eq 'refs/heads/master') {
+    $version = "1.1.$commitCount"
+    Write-Host "##vso[task.setvariable variable=NugetVersionType;]release"
+}
+if ($version -ne '') {
+    md _nuget
+    nuget pack $repoRoot\ECMA2Yaml\ECMA2Yaml\Nuget\ECMA2Yaml.nuspec -outputdirectory _nuget\ECMA2Yaml -version $version -Prop Configuration=Release
+    nuget pack $repoRoot\ECMA2Yaml\ECMAHelper\ECMAHelper.csproj -outputdirectory _nuget\ECMAHelper -version $version -Prop Configuration=Release
+    Write-Host "##vso[build.addbuildtag]$version"
+    Write-Host "##vso[task.setvariable variable=NeedNugetPush;]Yes"
+}
