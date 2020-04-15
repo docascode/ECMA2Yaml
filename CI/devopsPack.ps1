564370eda12b3b1451508e1523e7f1ac6fa420c2
Tianqi Zhang <tianzh@microsoft.com>
2020-04-15 16:17:56 +0800

    fix appveyor again

--- CI/devopsPack.ps1
+++ CI/devopsPack.ps1
@@ -12,7 +12,7 @@ if (("$env:BUILD_REASON" -eq "Manual" -or "$env:BUILD_REASON" -eq "IndividualCI"
     $version = '1.1.$commitCount.$env:BUILD_BUILDNUMBER'
     Write-Host "##vso[task.setvariable variable=NugetVersionType;]release"
 }
-if ($revision -ne '') {
+if ($version -ne '') {
     md _nuget
     nuget pack $repoRoot\ECMA2Yaml\ECMA2Yaml\Nuget\ECMA2Yaml.nuspec -outputdirectory _nuget\ECMA2Yaml -version $version -Prop Configuration=Release
     nuget pack $repoRoot\ECMA2Yaml\ECMAHelper\ECMAHelper.csproj -outputdirectory _nuget\ECMAHelper -version $version -Prop Configuration=Release
