8527189c5296716c0cdc3f35eeb3c0851fe2b979
Tianqi Zhang <tianzh@microsoft.com>
2020-04-15 14:33:11 +0800

    move CI scripts

---
+++ CI/devopsPack.ps1
@@ -0,0 +1,18 @@
+$revision = ""
+$commitCount = & { git rev-list --count HEAD }
+$revision = ''
+
+if ("$(Build.Reason)" -eq "PullRequest" -or "$env:BUILD_SOURCEBRANCH" -eq 'refs/heads/develop') {
+    $revision = '1.1.$commitCount.$(Build.BuildNumber)-beta'
+    Write-Host "prerelease"
+}
+if (("$(Build.Reason)" -eq "Manual" -or "$(Build.Reason)" -eq "IndividualCI") `
+    -and "$env:BUILD_SOURCEBRANCH" -eq 'refs/heads/devops') {
+    $revision = '1.1.$commitCount.$(Build.BuildNumber)'
+    Write-Host "release"
+}
+if ($revision -neq '') {
+    md _nuget
+    nuget pack ..\ECMA2Yaml\ECMA2Yaml\Nuget\ECMA2Yaml.nuspec -outputdirectory _nuget -version $revision -Prop Configuration=Release
+    nuget pack ..\ECMA2Yaml\ECMAHelper\ECMAHelper.csproj -outputdirectory _nuget -version $revision -Prop Configuration=Release
+}
