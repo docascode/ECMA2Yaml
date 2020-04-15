6a52b0db8eb7c906c56db194ec92b536a1c53a3b
Tianqi Zhang <tianzh@microsoft.com>
2020-04-15 15:41:50 +0800

    fix nuget pack path

--- CI/devopsIntTest.ps1
+++ CI/devopsIntTest.ps1
@@ -2,10 +2,10 @@ $ErrorActionPreference = "Stop"
 
 Write-Host "Start integration test..."
 
-$currentDir = $($MyInvocation.MyCommand.Definition) | Split-Path | Split-Path
+$repoRoot = $($MyInvocation.MyCommand.Definition) | Split-Path | Split-Path
 
-& "$currentDir/ECMA2Yaml/IntellisenseFileGen/bin/Release/IntellisenseFileGen.exe" -x $currentDir\test\xml -d $currentDir\test -o $currentDir\test\intellisense_After
-& "$currentDir/ECMA2Yaml/DiffFiles/bin/Release/DiffFiles.exe" -o $currentDir\test\intellisense -n $currentDir\test\intellisense_After -l $currentDir\test --Path
+& "$repoRoot/ECMA2Yaml/IntellisenseFileGen/bin/Release/IntellisenseFileGen.exe" -x $repoRoot\test\xml -d $repoRoot\test -o $repoRoot\test\intellisense_After
+& "$repoRoot/ECMA2Yaml/DiffFiles/bin/Release/DiffFiles.exe" -o $repoRoot\test\intellisense -n $repoRoot\test\intellisense_After -l $repoRoot\test --Path
 if ($LASTEXITCODE -ne 0)
 {
     Write-Error "Diff found for intellisense."
@@ -14,8 +14,8 @@ else {
     Write-Host "Done testing intellisense..."
 }
 
-& "$currentDir/ECMA2Yaml/ECMA2Yaml/bin/Release/ECMA2Yaml.exe" -s $currentDir\test\xml -o $currentDir\test\yml_MREF_After -f --repoRoot $currentDir\\ --repoBranch master --repoUrl https://github.com/docascode/ECMA2Yaml
-& "$currentDir/ECMA2Yaml/DiffFiles/bin/Release/DiffFiles.exe" -o $currentDir\test\yml_MREF -n $currentDir\test\yml_MREF_After -l $currentDir\test --Path
+& "$repoRoot/ECMA2Yaml/ECMA2Yaml/bin/Release/ECMA2Yaml.exe" -s $repoRoot\test\xml -o $repoRoot\test\yml_MREF_After -f --repoRoot $repoRoot\\ --repoBranch master --repoUrl https://github.com/docascode/ECMA2Yaml
+& "$repoRoot/ECMA2Yaml/DiffFiles/bin/Release/DiffFiles.exe" -o $repoRoot\test\yml_MREF -n $repoRoot\test\yml_MREF_After -l $repoRoot\test --Path
 if ($LASTEXITCODE -ne 0)
 {
     Write-Error "Diff found for MREF yml."
@@ -24,8 +24,8 @@ else {
     Write-Host "Done testing MREF yml..."
 }
 
-& "$currentDir/ECMA2Yaml/ECMA2Yaml/bin/Release/ECMA2Yaml.exe" -s $currentDir\test\xml -o $currentDir\test\yml_SDP_After --SDP -f --repoRoot $currentDir\\ --repoBranch master --repoUrl https://github.com/docascode/ECMA2Yaml
-& "$currentDir/ECMA2Yaml/DiffFiles/bin/Release/DiffFiles.exe" -o $currentDir\test\yml_SDP -n $currentDir\test\yml_SDP_After -l $currentDir\test --Path
+& "$repoRoot/ECMA2Yaml/ECMA2Yaml/bin/Release/ECMA2Yaml.exe" -s $repoRoot\test\xml -o $repoRoot\test\yml_SDP_After --SDP -f --repoRoot $repoRoot\\ --repoBranch master --repoUrl https://github.com/docascode/ECMA2Yaml
+& "$repoRoot/ECMA2Yaml/DiffFiles/bin/Release/DiffFiles.exe" -o $repoRoot\test\yml_SDP -n $repoRoot\test\yml_SDP_After -l $repoRoot\test --Path
 if ($LASTEXITCODE -ne 0)
 {
     Write-Error "Diff found for SDP yml."
--- CI/devopsPack.ps1
+++ CI/devopsPack.ps1
@@ -1,18 +1,21 @@
 $revision = ""
 $commitCount = & { git rev-list --count HEAD }
-$revision = ''
+$version = ''
+$repoRoot = $($MyInvocation.MyCommand.Definition) | Split-Path | Split-Path
 
 if ("$env:BUILD_REASON" -eq "PullRequest" -or "$env:BUILD_SOURCEBRANCH" -eq 'refs/heads/develop') {
-    $revision = '1.1.$commitCount.$env:BUILD_BUILDNUMBER-beta'
+    $version = '1.1.$commitCount.$env:BUILD_BUILDNUMBER-beta'
     Write-Host "prerelease"
 }
 if (("$env:BUILD_REASON" -eq "Manual" -or "$env:BUILD_REASON" -eq "IndividualCI") `
     -and "$env:BUILD_SOURCEBRANCH" -eq 'refs/heads/devops') {
-    $revision = '1.1.$commitCount.$env:BUILD_BUILDNUMBER'
+    $version = '1.1.$commitCount.$env:BUILD_BUILDNUMBER'
     Write-Host "release"
 }
 if ($revision -ne '') {
     md _nuget
-    nuget pack ..\ECMA2Yaml\ECMA2Yaml\Nuget\ECMA2Yaml.nuspec -outputdirectory _nuget -version $revision -Prop Configuration=Release
-    nuget pack ..\ECMA2Yaml\ECMAHelper\ECMAHelper.csproj -outputdirectory _nuget -version $revision -Prop Configuration=Release
+    nuget pack $repoRoot\ECMA2Yaml\ECMA2Yaml\Nuget\ECMA2Yaml.nuspec -outputdirectory _nuget -version $version -Prop Configuration=Release
+    nuget pack $repoRoot\ECMA2Yaml\ECMAHelper\ECMAHelper.csproj -outputdirectory _nuget -version $version -Prop Configuration=Release
+    Write-Host "##vso[build.addbuildtag]$version"
+    Write-Host "##vso[task.setvariable variable=BuildVersionNumber;]$version"
 }
