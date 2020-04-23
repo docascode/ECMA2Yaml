d8de90336271414e349fc2a4eb1751e700f675aa
Shuang Jiang <jiangshuang007@126.com>
2020-04-23 17:30:16 +0800

    Sync data from test repo (#98)
    * sync test data from test repo
    
    * update test data
    
    * dix
    
    * add publish artifacts
    
    * update
    
    * update test data again
    
    * update temp yaml folder
    
    * add isEii in test data
    
    Co-authored-by: Shuang Jiang <shujia@microsoft.com>

--- CI/devopsIntTest.ps1
+++ CI/devopsIntTest.ps1
@@ -4,8 +4,8 @@ Write-Host "Start integration test..."
 
 $repoRoot = $($MyInvocation.MyCommand.Definition) | Split-Path | Split-Path
 
-& "$repoRoot/ECMA2Yaml/IntellisenseFileGen/bin/Release/IntellisenseFileGen.exe" -x $repoRoot\test\xml -d $repoRoot\test -o $repoRoot\test\intellisense_After
-& "$repoRoot/ECMA2Yaml/DiffFiles/bin/Release/DiffFiles.exe" -o $repoRoot\test\intellisense -n $repoRoot\test\intellisense_After -l $repoRoot\test --Path
+& "$repoRoot/ECMA2Yaml/IntellisenseFileGen/bin/Release/IntellisenseFileGen.exe" -x $repoRoot\test\xml -d $repoRoot\test -o $repoRoot\test\_intellisense_After
+& "$repoRoot/ECMA2Yaml/DiffFiles/bin/Release/DiffFiles.exe" -o $repoRoot\test\intellisense -n $repoRoot\test\_intellisense_After -l $repoRoot\test --Path
 if ($LASTEXITCODE -ne 0)
 {
     Write-Error "Diff found for intellisense."
@@ -14,8 +14,8 @@ else {
     Write-Host "Done testing intellisense..."
 }
 
-& "$repoRoot/ECMA2Yaml/ECMA2Yaml/bin/Release/ECMA2Yaml.exe" -s $repoRoot\test\xml -o $repoRoot\test\yml_MREF_After -f --repoRoot $repoRoot\\ --repoBranch master --repoUrl https://github.com/docascode/ECMA2Yaml
-& "$repoRoot/ECMA2Yaml/DiffFiles/bin/Release/DiffFiles.exe" -o $repoRoot\test\yml_MREF -n $repoRoot\test\yml_MREF_After -l $repoRoot\test --Path
+& "$repoRoot/ECMA2Yaml/ECMA2Yaml/bin/Release/ECMA2Yaml.exe" -s $repoRoot\test\xml -o $repoRoot\test\_yml_MREF_After -f --repoRoot $repoRoot\\ --repoBranch master --repoUrl https://github.com/docascode/ECMA2Yaml
+& "$repoRoot/ECMA2Yaml/DiffFiles/bin/Release/DiffFiles.exe" -o $repoRoot\test\yml_MREF -n $repoRoot\test\_yml_MREF_After -l $repoRoot\test --Path
 if ($LASTEXITCODE -ne 0)
 {
     Write-Error "Diff found for MREF yml."
@@ -24,14 +24,24 @@ else {
     Write-Host "Done testing MREF yml..."
 }
 
-& "$repoRoot/ECMA2Yaml/ECMA2Yaml/bin/Release/ECMA2Yaml.exe" -s $repoRoot\test\xml -o $repoRoot\test\yml_SDP_After --SDP -f --repoRoot $repoRoot\\ --repoBranch master --repoUrl https://github.com/docascode/ECMA2Yaml
-& "$repoRoot/ECMA2Yaml/DiffFiles/bin/Release/DiffFiles.exe" -o $repoRoot\test\yml_SDP -n $repoRoot\test\yml_SDP_After -l $repoRoot\test --Path
+& "$repoRoot/ECMA2Yaml/ECMA2Yaml/bin/Release/ECMA2Yaml.exe" -s $repoRoot\test\xml -o $repoRoot\test\_yml_SDP_After --SDP -f --repoRoot $repoRoot\\ --repoBranch master --repoUrl https://github.com/docascode/ECMA2Yaml
+& "$repoRoot/ECMA2Yaml/DiffFiles/bin/Release/DiffFiles.exe" -o $repoRoot\test\yml_SDP -n $repoRoot\test\_yml_SDP_After -l $repoRoot\test --Path
 if ($LASTEXITCODE -ne 0)
 {
     Write-Error "Diff found for SDP yml."
 }
 else {
     Write-Host "Done testing SDP yml..."
+}
+
+& "$repoRoot/ECMA2Yaml/ECMA2Yaml/bin/Release/ECMA2Yaml.exe" -s $repoRoot\test\xml -o $repoRoot\test\_yml_UWP_SDP_After --UWP --SDP -f --repoRoot $repoRoot\\ --repoBranch master --repoUrl https://github.com/docascode/ECMA2Yaml
+& "$repoRoot/ECMA2Yaml/DiffFiles/bin/Release/DiffFiles.exe" -o $repoRoot\test\yml_UWP_SDP -n $repoRoot\test\_yml_UWP_SDP_After -l $repoRoot\test --Path
+if ($LASTEXITCODE -ne 0)
+{
+    Write-Error "Diff found for UWP SDP yml."
+}
+else {
+    Write-Host "Done testing UWP SDP yml..."
 }
 
 Write-Host "Finished integration test..."
