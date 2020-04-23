39eb8a72007cfbc5930c775082deb9a1856d34c0
Shuang Jiang <shujia@microsoft.com>
2020-04-23 16:58:24 +0800

    update temp yaml folder

--- test/gen-test-yaml.ps1
+++ test/gen-test-yaml.ps1
@@ -4,8 +4,8 @@ Write-Host "Start generating yamls for test repo..."
 
 $repoRoot = $($MyInvocation.MyCommand.Definition) | Split-Path | Split-Path
 
-& "$repoRoot/ECMA2Yaml/ECMA2Yaml/bin/Release/ECMA2Yaml.exe" -s $repoRoot\test\xml -o $repoRoot\test\yml_demo --demo -f --repoRoot $repoRoot\\ --repoBranch master --repoUrl https://github.com/docascode/ECMA2Yaml
+& "$repoRoot/ECMA2Yaml/ECMA2Yaml/bin/Release/ECMA2Yaml.exe" -s $repoRoot\test\xml -o $repoRoot\test\_yml_demo --demo -f --repoRoot $repoRoot\\ --repoBranch master --repoUrl https://github.com/docascode/ECMA2Yaml
 
-Remove-Item "$repoRoot\test\yml_demo" -Filter VersioningTest* -Force -ErrorAction Ignore -Recurse
+Remove-Item "$repoRoot\test\_yml_demo" -Filter VersioningTest* -Force -ErrorAction Ignore -Recurse
 
-Write-Host "Finished. Yamls generated in $repoRoot\test\yml_demo"
+Write-Host "Finished. Yamls generated in $repoRoot\test\_yml_demo"
