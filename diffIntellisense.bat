fc8136582a067449acbec0f43e6148d14198134d
Tianqi Zhang <tianzh@microsoft.com>
2020-05-18 13:12:07 +0800

    Remove unused CI related .bat files

--- diffIntellisense.bat
+++
@@ -1,3 +0,0 @@
-set currentDir=%~dp0
-"./ECMA2Yaml/IntellisenseFileGen/bin/Release/IntellisenseFileGen.exe" -x %currentDir%test\xml -d %currentDir%test -o %currentDir%test\_intellisense_After
-"./ECMA2Yaml/DiffFiles/bin/Release/DiffFiles.exe" -o %currentDir%test\intellisense -n %currentDir%test\_intellisense_After -l %currentDir%test --Path
