f77376515695426ea6d545f47b45e8ddf90b8a31
v-susu <v-susu@microsoft.com>
2020-05-25 10:55:22 +0800

    Add MSDNUrlPatch tool to fix old msdn links

---
+++ ECMA2Yaml/MSDNUrlPatch/CommandLineOptions.cs
@@ -0,0 +1,45 @@
+﻿using Mono.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSDNUrlPatch
+{
+    public class CommandLineOptions
+    {
+        public string SourceFolder { get; set; }
+        public string LogFilePath { get; set; }
+        public bool LogVerbose { get; set; }
+
+        List<string> Extras = null;
+        OptionSet _options = null;
+
+        public CommandLineOptions()
+        {
+            _options = new OptionSet {
+                { "s|source=", "[Required] the folder path containing the ECMAXML files.", s => SourceFolder = s },
+                { "l|log=", "[Required] the log file path.",  l => LogFilePath = l },
+                { "Ver", "Log verbose",  s => LogVerbose = s != null },
+            };
+        }
+
+        public bool Parse(string[] args)
+        {
+            Extras = _options.Parse(args);
+            if (string.IsNullOrEmpty(SourceFolder) || string.IsNullOrEmpty(LogFilePath))
+            {
+                PrintUsage();
+                return false;
+            }
+            return true;
+        }
+
+        private void PrintUsage()
+        {
+            Console.WriteLine("Usage: MSDNUrlPatch.exe <Options>");
+            _options.WriteOptionDescriptions(Console.Out);
+        }
+    }
+}
---
+++ ECMA2Yaml/UnitTest/MSDNUrlPatchTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MSDNUrlPatch;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Xml.Linq;
+
+
+namespace UnitTest
+{
+    [TestClass]
+    public class MSDNUrlPatchTests
+    {
+        [DataTestMethod]
+        [DataRow(@"https://docs.microsoft.com/en-us/dotnet/api/system.object?test=vb&view=netcore-3.1"
+                    , "https://docs.microsoft.com/en-us/dotnet/api/system.object?test=vb&view=netcore-3.1")]
+        [DataRow(@"https://docs.microsoft.com/en-us/previous-versions/ms180941(v=vs.90)?redirectedfrom=MSDN"
+                    , "https://docs.microsoft.com/en-us/previous-versions/ms180941(v=vs.90)")]
+        [DataRow("https://docs.microsoft.com/en-us/dotnet/api/system.object?redirectedfrom=MSDN&view=netcore-3.1"
+                    , "https://docs.microsoft.com/en-us/dotnet/api/system.object?view=netcore-3.1")]
+        [DataRow("https://docs.microsoft.com/en-us/dotnet/api/system.object?test=vb&redirectedfrom=MSDN&view=netcore-3.1"
+                    , "https://docs.microsoft.com/en-us/dotnet/api/system.object?test=vb&view=netcore-3.1")]
+        public void RemoveRedirectFromPart_Test(string inText, string expected)
+        {
+            var newUrl = new UrlRepaireHelper().RemoveUnusePartFromRedirectUrl(inText);
+            Assert.AreEqual(expected, newUrl);
+        }
+    }
+}
