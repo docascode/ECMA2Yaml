fc51965b11013a01eaa9181d154b2d7525d05fdf
v-susu <v-susu@microsoft.com>
2020-05-29 10:00:53 +0800

    update test data

--- ECMA2Yaml/UnitTest/MSDNUrlPatchTests.cs
+++ ECMA2Yaml/UnitTest/MSDNUrlPatchTests.cs
@@ -12,18 +12,18 @@ namespace UnitTest
     public class MSDNUrlPatchTests
     {
         [DataTestMethod]
-        [DataRow(@"https://docs.microsoft.com/en-us/dotnet/api/system.object?test=vb&view=netcore-3.1"
-                    , "/dotnet/api/system.object?test=vb&view=netcore-3.1")]
+        [DataRow(@"https://docs.microsoft.com/en-us/dotnet/api/system.object?test=vb&view1=netcore-3.1"
+                    , "https://docs.microsoft.com/en-us/dotnet/api/system.object?test=vb&view1=netcore-3.1")]
         [DataRow(@"https://docs.microsoft.com/en-us/previous-versions/ms180941(v=vs.90)?redirectedfrom=MSDN"
-                    , "/previous-versions/ms180941(v=vs.90)")]
-        [DataRow("https://docs.microsoft.com/en-us/dotnet/api/system.object?redirectedfrom=MSDN&view=netcore-3.1"
-                    , "/dotnet/api/system.object?view=netcore-3.1")]
-        [DataRow("https://docs.microsoft.com/en-us/dotnet/api/system.object?test=vb&redirectedfrom=MSDN&view=netcore-3.1"
-                    , "/dotnet/api/system.object?test=vb&view=netcore-3.1")]
-        [DataRow("https://docs.microsoft.com/en-us/visualstudio/msbuild/aspnetcompiler-task?view=vs-2015&redirectedfrom=MSDN"
-                    , "/visualstudio/msbuild/aspnetcompiler-task?view=vs-2015")]
-        [DataRow("https://docs1.microsoft.com/en-us/visualstudio/msbuild/aspnetcompiler-task?view=vs-2015&redirectedfrom=MSDN"
-                    , "https://docs1.microsoft.com/en-us/visualstudio/msbuild/aspnetcompiler-task?view=vs-2015")]
+                    , "https://docs.microsoft.com/en-us/previous-versions/ms180941(v=vs.90)")]
+        [DataRow("https://docs.microsoft.com/en-us/dotnet/api/system.object?redirectedfrom=MSDN&view1=netcore-3.1"
+                    , "https://docs.microsoft.com/en-us/dotnet/api/system.object?view1=netcore-3.1")]
+        [DataRow("https://docs.microsoft.com/en-us/dotnet/api/system.object?test=vb&redirectedfrom=MSDN&view1=netcore-3.1"
+                    , "https://docs.microsoft.com/en-us/dotnet/api/system.object?test=vb&view1=netcore-3.1")]
+        [DataRow("https://docs.microsoft.com/en-us/visualstudio/msbuild/aspnetcompiler-task?view1=vs-2015&redirectedfrom=MSDN"
+                    , "https://docs.microsoft.com/en-us/visualstudio/msbuild/aspnetcompiler-task?view1=vs-2015")]
+        [DataRow("https://docs1.microsoft.com/en-us/visualstudio/msbuild/aspnetcompiler-task?view1=vs-2015&redirectedfrom=MSDN"
+                    , "https://docs1.microsoft.com/en-us/visualstudio/msbuild/aspnetcompiler-task?view1=vs-2015")]
         public void RemoveRedirectFromPart_Test(string inText, string expected)
         {
             var newUrl = new UrlRepaireHelper().RemoveUnusePartFromRedirectUrl(inText);
