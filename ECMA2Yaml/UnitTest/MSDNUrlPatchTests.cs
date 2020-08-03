efc6ea4b0de9f11fc7a04bd619ef5a5770f22c67
luyajun0205 <v-yajlu@microsoft.com>
2020-08-03 14:18:59 +0800

    fix MSDNUrlPatch tool bugs (#139)
    * fix MSDNUrlPatch tool bugs
    
    * update
    
    Co-authored-by: v-susu <v-susu@microsoft.com>

--- ECMA2Yaml/UnitTest/MSDNUrlPatchTests.cs
+++ ECMA2Yaml/UnitTest/MSDNUrlPatchTests.cs
@@ -34,7 +34,31 @@ namespace UnitTest
         }
 
         [DataTestMethod]
-        [DataRow("https://msdn.microsoft.com/library/ex0ss12c(VS.80).aspx", "NoNeed")]
+        [DataRow(@"https://msdn.microsoft.com/library/e5ae402f-6dda-4732-bbe8-77296630f675"
+                    , "/previous-versions/h846e9b3(v=vs.110)",true)]
+        [DataRow(@"https://msdn.microsoft.com/library/e5ae402f-6dda-4732-bbe8-77296630f675"
+                    , "NoNeed", false)]
+        public void PreVersions_Switch_Test(string inText, string expected, bool isPreVersions)
+        {
+            CommandLineOptions option = new CommandLineOptions() { BaseUrl = "https://docs.microsoft.com/en-us", PreVersions=isPreVersions };
+            var newUrl = new UrlRepairHelper(option).GetDocsUrl(inText);
+            Assert.AreEqual(expected, newUrl);
+        }
+
+        [DataTestMethod]
+        [DataRow(@"https://msdn.microsoft.com/library/f811c019-a67b-4d54-82e6-e29549496f6e"
+                    , "/visualstudio/msbuild/aspnetcompiler-task?view=vs-2015", true)]
+        [DataRow(@"https://msdn.microsoft.com/library/f811c019-a67b-4d54-82e6-e29549496f6e"
+                    , "NoNeed", false)]
+        public void FixedVersions_Switch_Test(string inText, string expected, bool isFixedVersions)
+        {
+            CommandLineOptions option = new CommandLineOptions() { BaseUrl = "https://docs.microsoft.com/en-us",  FixedVersions = isFixedVersions };
+            var newUrl = new UrlRepairHelper(option).GetDocsUrl(inText);
+            Assert.AreEqual(expected, newUrl);
+        }
+
+        [DataTestMethod]
+        [DataRow("https://msdn.microsoft.com", "https://docs.microsoft.com")]
         public void GetDocsUrl_Test(string inText, string expected)
         {
             CommandLineOptions option = new CommandLineOptions() { BaseUrl = "https://docs.microsoft.com/en-us" };
@@ -43,12 +67,21 @@ namespace UnitTest
         }
 
         [DataTestMethod]
-        [DataRow("- [WinHttp](https://msdn.microsoft.com/library/windows/desktop/aa382925(v=vs.85).aspx) logs")]
-        public void GetMSDNUrls_Test(string fileText)
+        [DataRow("ms:mtpsurl: https://msdn.microsoft.com/library/microsoft.web.management.databasemanager.column.allownulls"
+            , "ms:mtpsurl: https://msdn.microsoft.com/library/microsoft.web.management.databasemanager.column.allownulls")]
+        [DataRow("mstest:mtpsurl: https://msdn.microsoft.com/library/microsoft.web.management.databasemanager.column.allownulls"
+            , "mstest:mtpsurl: /iis/extensions/database-manager-reference/column-allownulls-property-microsoft-web-management-databasemanager")]
+        [DataRow("This is a page [test](https://msdn.microsoft.com/library/0eee8ced-ad68-427d-b95a-97260e98deed) for test"
+            , "This is a page <https://docs.microsoft.com/en-us/dotnet/framework/configure-apps/file-schema/wcf/wshttpbinding> for test")]
+        [DataRow("the Forms Authentication module (see docs at [https://msdn.microsoft.com/library/1d3t3c61.aspx](https://msdn.microsoft.com/library/1d3t3c61.aspx))"
+            , "the Forms Authentication module (see docs at <https://docs.microsoft.com/en-us/previous-versions/dotnet/netframework-4.0/1d3t3c61(v=vs.100)>)")]
+        [DataRow("see [Code First Data Annotations](https://msdn.microsoft.com/library/jj591583(v=vs.113).aspx)"
+               , "see <https://docs.microsoft.com/en-us/ef/ef6/modeling/code-first/data-annotations>")]
+        public void RepairString_Test(string inText, string expected)
         {
-            CommandLineOptions option = new CommandLineOptions() { BaseUrl = "https://docs.microsoft.com/en-us" };
-            var urls = new UrlRepairHelper(option).GetMSDNUrls(fileText);
-            Assert.AreEqual(1, urls.Count());
+            CommandLineOptions option = new CommandLineOptions() { BaseUrl = "https://docs.microsoft.com/en-us" , FixedVersions= true, PreVersions=true };
+            var newText = new UrlRepairHelper(option).RepairString(inText, null);
+            Assert.AreEqual(expected, newText);
         }
     }
 }
