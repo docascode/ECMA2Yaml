25de7407384e0f3d12613c19f5bb36687968f781
v-susu <v-susu@microsoft.com>
2020-08-05 17:28:54 +0800

    resolve comments

--- ECMA2Yaml/UnitTest/MSDNUrlPatchTests.cs
+++ ECMA2Yaml/UnitTest/MSDNUrlPatchTests.cs
@@ -35,31 +35,29 @@ namespace UnitTest
 
         [DataTestMethod]
         [DataRow(@"https://msdn.microsoft.com/library/e5ae402f-6dda-4732-bbe8-77296630f675"
-                    , "/previous-versions/h846e9b3(v=vs.110)", true)]
+                    , "/previous-versions/h846e9b3(v=vs.110)", true, "https://docs.microsoft.com/en-us/previous-versions/h846e9b3(v=vs.110)?redirectedfrom=MSDN")]
         [DataRow(@"https://msdn.microsoft.com/library/e5ae402f-6dda-4732-bbe8-77296630f675"
-                    , "NoNeed", false)]
-        public void PreVersions_Switch_Test(string inText, string expected, bool isPreVersions)
+                    , "NoNeed", false, "https://docs.microsoft.com/en-us/previous-versions/h846e9b3(v=vs.110)?redirectedfrom=MSDN")]
+        public void PreVersions_Switch_Test(string inText, string expected, bool isPreVersions, string mockRedirectUrl)
         {
             CommandLineOptions option = new CommandLineOptions() { BaseUrl = "https://docs.microsoft.com/en-us", PreVersions=isPreVersions };
             var urlRepairHelper = new UrlRepairHelper(option);
-            urlRepairHelper.MockTestData("https://msdn.microsoft.com/library/e5ae402f-6dda-4732-bbe8-77296630f675"
-                , "https://docs.microsoft.com/en-us/previous-versions/h846e9b3(v=vs.110)?redirectedfrom=MSDN");
+            urlRepairHelper.MockTestData(inText, mockRedirectUrl);
             var newUrl = urlRepairHelper.GetDocsUrl(inText);
             Assert.AreEqual(expected, newUrl);
         }
 
         [DataTestMethod]
         [DataRow(@"https://msdn.microsoft.com/library/f811c019-a67b-4d54-82e6-e29549496f6e"
-                    , "/visualstudio/msbuild/aspnetcompiler-task?view=vs-2015", true)]
+                    , "/visualstudio/msbuild/aspnetcompiler-task?view=vs-2015", true, "https://docs.microsoft.com/en-us/visualstudio/msbuild/aspnetcompiler-task?view=vs-2015&redirectedfrom=MSDN")]
         [DataRow(@"https://msdn.microsoft.com/library/f811c019-a67b-4d54-82e6-e29549496f6e"
-                    , "NoNeed", false)]
-        public void FixedVersions_Switch_Test(string inText, string expected, bool isFixedVersions)
+                    , "NoNeed", false, "https://docs.microsoft.com/en-us/visualstudio/msbuild/aspnetcompiler-task?view=vs-2015&redirectedfrom=MSDN")]
+        public void FixedVersions_Switch_Test(string inText, string expected, bool isFixedVersions, string mockRedirectUrl)
         {
             CommandLineOptions option = new CommandLineOptions() { BaseUrl = "https://docs.microsoft.com/en-us",  FixedVersions = isFixedVersions };
             var urlRepairHelper = new UrlRepairHelper(option);
-            urlRepairHelper.MockTestData("https://msdn.microsoft.com/library/f811c019-a67b-4d54-82e6-e29549496f6e"
-                , "https://docs.microsoft.com/en-us/visualstudio/msbuild/aspnetcompiler-task?view=vs-2015&redirectedfrom=MSDN");
-            var newUrl = new UrlRepairHelper(option).GetDocsUrl(inText);
+            urlRepairHelper.MockTestData(inText, mockRedirectUrl);
+            var newUrl = urlRepairHelper.GetDocsUrl(inText);
             Assert.AreEqual(expected, newUrl);
         }
 
@@ -74,26 +72,31 @@ namespace UnitTest
 
         [DataTestMethod]
         [DataRow("ms:mtpsurl: https://msdn.microsoft.com/library/microsoft.web.management.databasemanager.column.allownulls"
-            , "ms:mtpsurl: https://msdn.microsoft.com/library/microsoft.web.management.databasemanager.column.allownulls")]
+            , "ms:mtpsurl: https://msdn.microsoft.com/library/microsoft.web.management.databasemanager.column.allownulls"
+            , "https://msdn.microsoft.com/library/microsoft.web.management.databasemanager.column.allownulls"
+            , "https://docs.microsoft.com/en-us/iis/extensions/database-manager-reference/column-allownulls-property-microsoft-web-management-databasemanager?redirectedfrom=MSDN")]
         [DataRow("mstest:mtpsurl: https://msdn.microsoft.com/library/microsoft.web.management.databasemanager.column.allownulls"
-            , "mstest:mtpsurl: /iis/extensions/database-manager-reference/column-allownulls-property-microsoft-web-management-databasemanager")]
+            , "mstest:mtpsurl: /iis/extensions/database-manager-reference/column-allownulls-property-microsoft-web-management-databasemanager"
+            , "https://msdn.microsoft.com/library/microsoft.web.management.databasemanager.column.allownulls"
+            , "https://docs.microsoft.com/en-us/iis/extensions/database-manager-reference/column-allownulls-property-microsoft-web-management-databasemanager?redirectedfrom=MSDN")]
+
         [DataRow("This is a page [test](https://msdn.microsoft.com/library/0eee8ced-ad68-427d-b95a-97260e98deed) for test"
-            , "This is a page <https://docs.microsoft.com/en-us/dotnet/framework/configure-apps/file-schema/wcf/wshttpbinding> for test")]
+            , "This is a page <https://docs.microsoft.com/en-us/dotnet/framework/configure-apps/file-schema/wcf/wshttpbinding> for test"
+            , "https://msdn.microsoft.com/library/0eee8ced-ad68-427d-b95a-97260e98deed"
+            , "https://docs.microsoft.com/en-us/dotnet/framework/configure-apps/file-schema/wcf/wshttpbinding?redirectedfrom=MSDN")]
         [DataRow("the Forms Authentication module (see docs at [https://msdn.microsoft.com/library/1d3t3c61.aspx](https://msdn.microsoft.com/library/1d3t3c61.aspx))"
-            , "the Forms Authentication module (see docs at <https://docs.microsoft.com/en-us/previous-versions/dotnet/netframework-4.0/1d3t3c61(v=vs.100)>)")]
+            , "the Forms Authentication module (see docs at <https://docs.microsoft.com/en-us/previous-versions/dotnet/netframework-4.0/1d3t3c61(v=vs.100)>)"
+            , "https://msdn.microsoft.com/library/1d3t3c61.aspx"
+            , "https://docs.microsoft.com/en-us/previous-versions/dotnet/netframework-4.0/1d3t3c61(v=vs.100)?redirectedfrom=MSDN")]
         [DataRow("see [Code First Data Annotations](https://msdn.microsoft.com/library/jj591583(v=vs.113).aspx)"
-               , "see <https://docs.microsoft.com/en-us/ef/ef6/modeling/code-first/data-annotations>")]
-        public void RepairString_Test(string inText, string expected)
+            , "see <https://docs.microsoft.com/en-us/ef/ef6/modeling/code-first/data-annotations>"
+            , "https://msdn.microsoft.com/library/jj591583(v=vs.113).aspx"
+            , "https://docs.microsoft.com/en-us/ef/ef6/modeling/code-first/data-annotations?redirectedfrom=MSDN")]
+        public void RepairString_Test(string inText, string expected, string msdnUrl, string mockRedirectUrl)
         {
             CommandLineOptions option = new CommandLineOptions() { BaseUrl = "https://docs.microsoft.com/en-us" , FixedVersions= true, PreVersions=true };
             var urlRepairHelper = new UrlRepairHelper(option);
-            urlRepairHelper.MockTestData("https://msdn.microsoft.com/library/microsoft.web.management.databasemanager.column.allownulls"
-                , "https://docs.microsoft.com/en-us/iis/extensions/database-manager-reference/column-allownulls-property-microsoft-web-management-databasemanager?redirectedfrom=MSDN");
-            urlRepairHelper.MockTestData("https://msdn.microsoft.com/library/1d3t3c61.aspx"
-                , "https://docs.microsoft.com/en-us/previous-versions/dotnet/netframework-4.0/1d3t3c61(v=vs.100)?redirectedfrom=MSDN");
-            urlRepairHelper.MockTestData("https://msdn.microsoft.com/library/jj591583(v=vs.113).aspx"
-                , "https://docs.microsoft.com/en-us/ef/ef6/modeling/code-first/data-annotations?redirectedfrom=MSDN");
-            urlRepairHelper.MockTestData("", "");
+            urlRepairHelper.MockTestData(msdnUrl, mockRedirectUrl);
             var newText = urlRepairHelper.RepairString(inText, null);
             Assert.AreEqual(expected, newText);
         }
